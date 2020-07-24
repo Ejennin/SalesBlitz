743ff7b86859c95416644d0c837cdbb179b9da78
Ejennin <ericejennings@gmail.com>
2020-07-24 18:29:04 -0400

    save for db

---
+++ dbo.Blitz.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Blitz] (
+    [BlitzId]    INT                IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX)     NOT NULL,
+    [Location]   NVARCHAR (MAX)     NOT NULL,
+    [Date]       DATETIME           NOT NULL,
+    [CreatedUtc] DATETIMEOFFSET (7) NOT NULL,
+    [Url]        NVARCHAR (MAX)     NULL,
+    CONSTRAINT [PK_dbo.Blitz] PRIMARY KEY CLUSTERED ([BlitzId] ASC)
+);
+
---
+++ dbo.RepsAtBlitz.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[RepsAtBlitz] (
+    [RepsId]   INT            IDENTITY (1, 1) NOT NULL,
+    [RepName]  NVARCHAR (MAX) NULL,
+    [RepId]    INT            NULL,
+    [BlitzId]  INT            NULL,
+    [Position] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.RepsAtBlitz] PRIMARY KEY CLUSTERED ([RepsId] ASC),
+    CONSTRAINT [FK_dbo.RepsAtBlitz_dbo.Blitz_BlitzId] FOREIGN KEY ([BlitzId]) REFERENCES [dbo].[Blitz] ([BlitzId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.RepsAtBlitz_dbo.Rep_RepId] FOREIGN KEY ([RepId]) REFERENCES [dbo].[Rep] ([RepId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_RepId]
+    ON [dbo].[RepsAtBlitz]([RepId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BlitzId]
+    ON [dbo].[RepsAtBlitz]([BlitzId] ASC);
+
